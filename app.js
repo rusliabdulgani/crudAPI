b3b4d9e446ef20c067e0e3e539a819d36e1dcc9f
abdulgani88 <rusli.gani88@gmail.com>
2020-06-11 01:28:12 +0700

    add delete

--- app.js
+++ app.js
@@ -46,6 +46,16 @@ app.post('/postData', async (req, res) => {
   }
 })
 
+app.delete('/deleteData', async (req, res) => {
+  let { id } = req.body
+  try {
+    let data = await sequelize.query(`delete from logistics where id=${id}`, { type: QueryTypes.SELECT })
+    res.send(data)
+  } catch (e) {
+    res.send(e)
+  }
+})
+
 
 
 app.listen(process.env.PORT || 3000)
