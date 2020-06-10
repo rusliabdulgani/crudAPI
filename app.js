458a8197778d62ea7e61d4bb63ee512dc429cccc
abdulgani88 <rusli.gani88@gmail.com>
2020-06-11 00:57:37 +0700

    add postgres

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://qrdnizpriwmume:bbcf84fd4c8e3e98a1433eddbb076e301eee1827d739f9aa049d2ecdc611a0b1@ec2-52-44-55-63.compute-1.amazonaws.com:5432/ddc696pgqhcb1r') // Example for postgres
+
+sequelize.authenticate()
+.then(() => {
+  console.log('Connection to database established successfully')
+})
+.catch(err => {
+  console.log('Unable to connect to database: ', err)
+})
 
 app.use(cors())
 app.use(bodyParser.json())
